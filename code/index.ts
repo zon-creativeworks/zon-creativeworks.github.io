e2881e43d35f2345b83bf3e92b6b43a6df3a17f2
Arjay Jones <arjay.jones@nlucis.dev>
2023-04-30 21:00:46 -0700

    bare minimum threejs only setup that also works on ios

--- code/index.ts
+++ code/index.ts
@@ -1,64 +1,65 @@
 /* DEPENDENCIES */
+import * as THREE from 'three';
 import Phaser from 'phaser';
 import * as Tone from 'tone';
 
 /* * SCENE MODULES * */
-import MainInterface from './views/MainInterface';
-import InputHandler from './components/manager/InputHandler';
+// import MainInterface from './views/MainInterface';
+// import InputHandler from './components/manager/InputHandler';
 
 /* * GLOBAL CANVAS REFS * */
 export const threeCanvas: HTMLCanvasElement = document.getElementById('three') as HTMLCanvasElement;
-export const phaseCanvas: HTMLCanvasElement = document.getElementById('phase') as HTMLCanvasElement;
-export const bezel = 13;
+// export const phaseCanvas: HTMLCanvasElement = document.getElementById('phase') as HTMLCanvasElement;
+// export const bezel = 13;
 
 const overlayEl = document.getElementsByTagName('overlay')[0] as HTMLDivElement;
 
 document.addEventListener('DOMContentLoaded', () => {
 
-  const phaseUIConfig = {
-    type: Phaser.CANVAS,
-    canvas: phaseCanvas,
-    antialias: true,
-    antialiasGL: true,
-    physics: {
-      default: 'matter',
-      matter: {
-        debug: true,
-        gravity: { x: 0, y: 0 },
-        // @ts-ignore - false error due to incomplete type declarations in Phaser;
-        plugins: {
-          attractors: true
-        }
-      }
-    },
-    scale: {
-      mode: Phaser.Scale.RESIZE,
-      autoCenter: Phaser.Scale.CENTER_BOTH,
-    },
-    transparent: true,
-    disableContextMenu: true,
-  };
+  // const phaseUIConfig = {
+  //   type: Phaser.CANVAS,
+  //   canvas: phaseCanvas,
+  //   antialias: true,
+  //   antialiasGL: true,
+  //   physics: {
+  //     default: 'matter',
+  //     matter: {
+  //       debug: true,
+  //       gravity: { x: 0, y: 0 },
+  //       // @ts-ignore - false error due to incomplete type declarations in Phaser;
+  //       plugins: {
+  //         attractors: true
+  //       }
+  //     }
+  //   },
+  //   scale: {
+  //     mode: Phaser.Scale.RESIZE,
+  //     autoCenter: Phaser.Scale.CENTER_BOTH,
+  //   },
+  //   transparent: true,
+  //   disableContextMenu: true,
+  // };
 
   // The Mandorla UI
-  const mainUI = new Phaser.Game(phaseUIConfig);
+  // const mainUI = new Phaser.Game(phaseUIConfig);
 
   // Scene used only for requesting permissions and launching to fullscreen
-  class ViewManager extends Phaser.Scene {
-    constructor() {super('ViewManager')}
-    init(): void { 
-      this.cameras.main.centerOn(0, 0);
+  // class ViewManager extends Phaser.Scene {
+  //   constructor() {super('ViewManager')}
+  //   init(): void { 
+  //     this.cameras.main.centerOn(0, 0);
 
-      // Sub-View Scenes
-      mainUI.scene.add('InputHandler', InputHandler);
-      mainUI.scene.add('MainInterface', MainInterface);
-    }
+  //     // Sub-View Scenes
+  //     mainUI.scene.add('InputHandler', InputHandler);
+  //     mainUI.scene.add('MainInterface', MainInterface);
+  //   }
 
-    create(): void {
-      const res = { w: window.innerWidth, h: window.innerHeight }
-    }
-  }
-  mainUI.scene.add('ViewManager', ViewManager);
-  mainUI.scene.run('ViewManager');
+  //   create(): void {
+  //     const res = { w: window.innerWidth, h: window.innerHeight }
+  //   }
+  // }
+  // mainUI.scene.add('ViewManager', ViewManager);
+  // mainUI.scene.run('ViewManager');
 
     // Audio T-Base
     const now = Tone.now();
@@ -98,57 +99,34 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     }, 1);
 
+    // Basic ThreeJS Setup
+    const scene = new THREE.Scene();
 
-  // Start the app when the user clicks on the overlay
-  const clickStart = () => {
-    Tone.start();
-    buttonClick.start();
-    backgroundNoise.start();
-    capacitorChargeUp.start();
-    staticPop.start('+0.25');
-    staticPop.stop('+0.50');
-    fadeInStatic();
+    const renderer = new THREE.WebGLRenderer({ canvas: threeCanvas, alpha: true, antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.toneMapping = THREE.ACESFilmicToneMapping;
+    renderer.toneMappingExposure = 0.8;
+    renderer.setClearColor(0x000000, 0.90);
 
-    // Immediately remove the inner div containing the click prompt
-    overlayEl.innerHTML = '';
+    const cCamera = new THREE.PerspectiveCamera(42, window.innerWidth / window.innerHeight, 1e-4, 1e4);
 
-    // Remove the start-up click event
-    removeClick();
+    const geo = new THREE.IcosahedronGeometry(2, 0);
+    const flatWhite = new THREE.MeshBasicMaterial({ color: 0xFFFFFF, reflectivity: 1.0 });
+    const mesh = new THREE.Mesh(geo, flatWhite);
+    mesh.position.set(0, 0, -10);
 
-    // TODO: Figure out how to keep things looking good in fullscreen...
-    // Enable fullscreen
-    // document.getElementById('app')?.requestFullscreen();
+    scene.add(mesh);
+    cCamera.lookAt(mesh.position);
 
-    // Fade-Out overlay / Fade-In MainInterface
-    let baseOpacity = 1;
-    const fadeIn = () => setTimeout(() => {
-      overlayEl.style.opacity = `${baseOpacity}`;
-      if (baseOpacity > 0) {
-        baseOpacity -= 0.06;
-        fadeIn();
-      } else {
-        overlayEl.remove();
-        mainUI.scene.run('MainInterface');
-      }
-    }, 12);
-    fadeIn();
-  };
-  let removeClick = () => {overlayEl.removeEventListener('pointerup', clickStart)};
+    const render = () => {
+      requestAnimationFrame(render);   // First
 
-  // First Interaction Inits
-  document.addEventListener('pointerdown', () => Tone.start());
-
-  // After Inits
-  overlayEl.addEventListener('pointerup', clickStart);
-
+      mesh.rotation.y += 0.1;
+      renderer.render(scene, cCamera); // Last
+    };
+    render();
 });
-
-console.debug(
-  window['chrome'].app, 
-  window['chrome'].app.getIsInstalled(), 
-);
-window['chrome'].app.installState((s) => { console.debug(s)})
-
 
 
 
@@ -156,7 +134,3 @@ window['chrome'].app.installState((s) => { console.debug(s)})
 // TODO: Setup IndexedDB
 // TODO: Setup Sensors Permissions and switches
 // TODO: Setup IPFS user look-up directory
-// TODO: Setup Canvas Tile Composite to account for egregiously tall mobile device displays that distort 3D rendering
-// TODO: Configure TiLLI animation to fade out when menu fades in;
-// TODO: and configure menu to slide into the center of the viewport before opening
-// TODO: Clean-up the tweens so they dont step over eachother; just one per animation sequence
