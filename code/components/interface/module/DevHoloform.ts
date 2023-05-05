a135c1316ea221682f888a78eca7b43f7585b573
zon-creativeworks <arjay.jones@nlucis.dev>
2023-05-05 14:21:53 -0700

    stubbed 3D components for holoform and confirmed they properly add to their parent mmv three.scene

--- code/components/interface/module/DevHoloform.ts
+++ code/components/interface/module/DevHoloform.ts
@@ -14,14 +14,16 @@ export default class DevHoloform extends Holoform {
 
     // Visual 2D Form
     const visual2D = this.scene.add.circle(0, 30, 12, 0x000000, 1).setStrokeStyle(3, 0xFF00FF);
-    this.add([label, visual2D]);
 
     const visual3D = new THREE.Group();
     const visualGeometry = new THREE.IcosahedronGeometry(2, 0);
     const visualMaterial = new THREE.MeshBasicMaterial({ color: 0xFF00FF, wireframe: true });
     const visualMesh = new THREE.Mesh(visualGeometry, visualMaterial);
     visual3D.add(visualMesh);
-    
+
+    // Marshall all 2D and 3D elements into the active MMV
+    this.add([label, visual2D]);
+    this.parent.scene3.add(visual3D);
   }
   override animate(): void {}
 }
