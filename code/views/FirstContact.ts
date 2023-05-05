03d41cf8812af170e389d822ccfb4148d6f742f1
zon-creativeworks <arjay.jones@nlucis.dev>
2023-05-05 16:38:22 -0700

    setup 2d camera

--- code/views/FirstContact.ts
+++ code/views/FirstContact.ts
@@ -1,3 +1,23 @@
 export default class FirstContact extends Phaser.Scene {
+  constructor() {super('FirstContact')};
+
+  // Globals
+  // ...
   
+
+  private camera: Phaser.Cameras.Scene2D.Camera;
+  init(): void {
+    this.cameras.main.centerOn(0, 0);
+    this.camera = this.cameras.main;
+  }
+  
+  preload(): void {}
+  
+  create(): void {}
+  
+  preUpdate(): void {}
+  
+  update(): void {}
+  
+  postUpdate(): void {}
 }
